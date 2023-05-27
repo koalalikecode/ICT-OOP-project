c5cea613e864f4cd5c08bb88fcaeef08b4cad3db
willy <dungnguyen260103@gmail.com>
2023-05-27 23:52:59 +0700

    + crawler dynasty

--- src/main/java/crawler/CharacterNKSCrawler.java
+++ src/main/java/crawler/CharacterNKSCrawler.java
@@ -8,6 +8,7 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+
 import java.io.IOException;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
--- src/main/java/crawler/CrawlRunner.java
+++ src/main/java/crawler/CrawlRunner.java
@@ -2,7 +2,9 @@ package crawler;
 
 public class CrawlRunner {
     public static void main(String[] args) {
-        CharacterNKSCrawler crawler1 = new CharacterNKSCrawler();
+//        CharacterNKSCrawler crawler1 = new CharacterNKSCrawler();
+//        crawler1.crawlAndSave();
+        DynastyWikiCrawler crawler1 = new DynastyWikiCrawler();
         crawler1.crawlAndSave();
     }
 }
